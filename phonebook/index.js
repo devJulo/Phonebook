0cbdf06f674750f684e29cdb5c838ff46afe0c67
devJulo <julienfrancais@outlook.fr>
2020-12-14 17:46:54 +0100

    server runnig on port 3001

--- phonebook/index.js
+++ phonebook/index.js
@@ -1,1 +1,33 @@
-console.log('hello World')
+const express = require('express')
+const app = express()
+
+let persons = [{
+    id: 1,
+    name: "Arto Hellas",
+    number: "6454616"
+},
+{
+    id: 2,
+    name: "Ada Lovelace",
+    number: "55266516"
+},
+{
+    id: 3,
+    name: "Dan Abramov",
+    number: "561651656"
+},
+{
+    id: 4,
+    name: "Maty Poppendick",
+    number: "2655165"
+}
+]
+
+app.get('/', (request, response) =>{
+    response.send('<h1>My first Backend Server</h1>')
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
