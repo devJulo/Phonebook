fac7b7262cd66ad9c1eed56c3893eec7fced51b8
devJulo <julienfrancais@outlook.fr>
2020-12-14 17:50:57 +0100

    server displaying value on npm run start

--- phonebook/index.js
+++ phonebook/index.js
@@ -27,6 +27,10 @@ app.get('/', (request, response) =>{
     response.send('<h1>My first Backend Server</h1>')
 })
 
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
